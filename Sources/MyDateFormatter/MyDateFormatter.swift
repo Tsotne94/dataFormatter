01247f374515f0d20df4d93668b9e57d40b570ff
tsotne chubinidze <cotne.2004chubinidze@gmail.com>
2024-12-08 01:10:07 +0400

    updated

--- Sources/MyDateFormatter/MyDateFormatter.swift
+++ Sources/MyDateFormatter/MyDateFormatter.swift
@@ -9,9 +9,14 @@ public class MyDateFormatter: DateFormatting {
     private let dateFormatter = Foundation.DateFormatter()
     
     public func format(_ value: String) -> String {
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.timeZone = TimeZone(identifier: "Asia/Tbilisi")
+        
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ"
+        
         if let date = dateFormatter.date(from: value) {
-            dateFormatter.dateFormat = "EEEE, d MMMM yyyy"
+            dateFormatter.dateFormat = "EEEE, d MMMM yyyy HH:mm:ss"
             return dateFormatter.string(from: date)
         } else {
             return "error"
